aebb84b3fc5259929082c25346d1328c22809aef
your-user <yourmail@example.com>
2021-02-07 14:45:35 -0300

    Adding options to visual code installation

--- post-installer.sh
+++ post-installer.sh
@@ -38,7 +38,7 @@ sudo apt install npm -y
 
 # ================== visual code ==================  #
 # (https://linuxize.com/post/how-to-install-visual-studio-code-on-ubuntu-20-04/)
-sudo apt update
+sudo apt update -qq
 sudo apt install software-properties-common apt-transport-https wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
